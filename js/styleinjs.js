f04833beaa17cd5f3d63fe760e7bc8f2d3bdca44
Tahsina2226 <tahsinatanvin274@gmail.com>
2024-10-05 20:35:52 +0600

    ttour-of-dom

---
+++ js/styleinjs.js
@@ -0,0 +1,17 @@
+const sections = document.querySelectorAll('section');
+// console.log(sections);
+for (const section of sections) {
+    // console.log(section);
+    section.style.border = '2px solid steelblue';
+    section.style.marginBottom = '5px';
+    section.style.borderRadius = '25px';
+    section.style.paddingLeft = '17px';
+    section.style.backgroundColor = 'lightgray';
+
+
+}
+// const placesContainer = document.getElementById('places-container');
+// placesContainer.style.backgroundColor = 'lightpink';
+
+const placesContainer = document.getElementById('places-Container');
+placesContainer.classList.add('yellow-bg');
